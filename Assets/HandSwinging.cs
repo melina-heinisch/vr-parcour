036c5b4142fdde9628f95bdf4fbdff6d6189d3ed
s381817 <melina.heinisch@stud-mail.uni-wuerzburg.de>
2023-08-10 14:03:07 +0200

    Improvement for walking/running

--- Assets/HandSwinging.cs
+++ Assets/HandSwinging.cs
@@ -16,8 +16,6 @@ public class HandSwinging : MonoBehaviour
     //Vector3 positions
     private Vector3 positionPreviousFrameLeftHand;
     private Vector3 positionPreviousFrameRightHand;
-    private Vector3 playerPositionPreviousFrame; 
-    private Vector3 playerPositionThisFrame;
     private Vector3 positionThisFrameLeftHand;
     private Vector3 positionThisFrameRigthHand;
     
@@ -28,9 +26,8 @@ public class HandSwinging : MonoBehaviour
     void Start()
     {
         //Set original Previous frame positions at start up
-        playerPositionPreviousFrame = transform.position;
-        positionPreviousFrameLeftHand = leftHand.transform.position;
-        positionPreviousFrameRightHand = rightHand.transform.position;
+        positionPreviousFrameLeftHand = leftHand.transform.localPosition;
+        positionPreviousFrameRightHand = rightHand.transform.localPosition;
         
     }
     
@@ -38,32 +35,22 @@ public class HandSwinging : MonoBehaviour
 
     void Update()
     {
-        if (VRHostSystem.GetLeftHandDevice()
-            .TryGetFeatureValue(CommonUsages.primary2DAxis,
-                out var thumbstickAxisValue)) //  where left (-1.0,0.0), right (1.0,0.0), up (0.0,1.0), down (0.0,-1.0)
-        {
-            //forwardDirection.transform.forward += new Vector3(thumbstickAxisValue.x, 0, thumbstickAxisValue.y);
-        }
-
         float yRotation = VRHostSystem.getXROriginGameObject().transform.eulerAngles.y;
         forwardDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
         
 
         //Get current positions of hands
-        positionThisFrameLeftHand = leftHand.transform.position;
-        positionThisFrameRigthHand = rightHand.transform.position;
+        positionThisFrameLeftHand = leftHand.transform.localPosition;
+        positionThisFrameRigthHand = rightHand.transform.localPosition;
+        
 
-        //position of Player
-        playerPositionThisFrame = transform.position;
-
-        //Get distance the hands and player has moved since the last frame
-        var playerDistanceMoved = Vector3.Distance(playerPositionThisFrame, playerPositionPreviousFrame);
+        //Get distance the hands have moved since the last frame
         var leftHandDistanceMoved = Vector3.Distance(positionPreviousFrameLeftHand, positionThisFrameLeftHand);
         var rightHandDistanceMoved = Vector3.Distance(positionPreviousFrameRightHand, positionThisFrameRigthHand);
 
-        //Add them up to get the hand speed from the user minus the movement of the player to neglegt the movement of the player
-        handSpeed = ((leftHandDistanceMoved - playerDistanceMoved) +
-                     (rightHandDistanceMoved - playerDistanceMoved)) * 2f;
+        //Add them up to get the hand speed from the user
+        handSpeed = ((leftHandDistanceMoved) +
+                     (rightHandDistanceMoved)) * 2f;
 
         if (Time.timeSinceLevelLoad > 1f)
             transform.position += forwardDirection.transform.forward * (handSpeed * speed * Time.deltaTime);
@@ -71,7 +58,6 @@ public class HandSwinging : MonoBehaviour
             //Set previous positions of hands for the next frame
         positionPreviousFrameLeftHand = positionThisFrameLeftHand; //Set player position previous frame
         positionPreviousFrameRightHand = positionThisFrameRigthHand;
-        playerPositionPreviousFrame = playerPositionThisFrame;
 
     }
 }
