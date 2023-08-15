53e0a0989615ea1662a05e69440104a07dd4972f
Lukas Polifke <lukas.polifke@stud-mail.uni-wuerzburg.de>
2023-08-15 10:53:32 +0200

    added data structure for scoreboard entries

---
+++ Assets/3DUI/scripts/ScoreBoardEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _3DUI.scripts
+{
+    public class ScoreBoardEntry : IComparable<ScoreBoardEntry>
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public float Time { get; private set; }
+
+        public ScoreBoardEntry(string id, string name, float time)
+        {
+            ID = id;
+            Name = name;
+            Time = time;
+        }
+
+        public ScoreBoardEntry(string name, float time) : this(new Guid().ToString(), name, time)
+        {
+            
+        }
+
+        public int CompareTo(ScoreBoardEntry other)
+        {
+            return this.Time.CompareTo(other.Time);
+        }
+    }
+}
