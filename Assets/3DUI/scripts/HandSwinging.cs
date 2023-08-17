a6f4a5b7f91419594080d5974438bc122c97161d
s381817 <melina.heinisch@stud-mail.uni-wuerzburg.de>
2023-08-17 12:31:06 +0200

    Nur y/z für laufen, capsule collider, laufen in controller Richtung Start für acceleration curve

--- Assets/3DUI/scripts/HandSwinging.cs
+++ Assets/3DUI/scripts/HandSwinging.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,7 @@ public class HandSwinging : MonoBehaviour
     public GameObject leftHand;
     public GameObject rightHand; 
     public GameObject forwardDirection;
+    public AnimationCurve highSpeedModeAccelerationCurve;
     
     //Vector3 positions
     private Vector3 positionPreviousFrameLeftHand;
@@ -26,8 +28,8 @@ public class HandSwinging : MonoBehaviour
     void Start()
     {
         //Set original Previous frame positions at start up
-        positionPreviousFrameLeftHand = leftHand.transform.localPosition;
-        positionPreviousFrameRightHand = rightHand.transform.localPosition;
+        positionPreviousFrameLeftHand = new Vector3(0, leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
+        positionPreviousFrameRightHand = new Vector3(0, rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
         
     }
     
@@ -35,13 +37,22 @@ public class HandSwinging : MonoBehaviour
 
     void Update()
     {
-        float yRotation = VRHostSystem.getXROriginGameObject().transform.eulerAngles.y;
+        // Old version: float yRotation = VRHostSystem.getXROriginGameObject().transform.eulerAngles.y;
+        float yRotationRight = rightHand.transform.eulerAngles.y;
+        float yRotationLeft = leftHand.transform.eulerAngles.y;
+        float yRotation = (float)(Math.Round(((yRotationRight + yRotationLeft) / 2) / 10.0)) * 10;
+
+        if ((yRotationLeft > 320 || yRotationRight > 320) && (yRotationLeft < 40 || yRotationRight < 40))
+            yRotation = 0;
+
+        Debug.Log("Right: " + yRotationRight);
+        Debug.Log("Left: " + yRotationLeft);
+        Debug.Log(yRotation);
         forwardDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
-        
 
         //Get current positions of hands
-        positionThisFrameLeftHand = leftHand.transform.localPosition;
-        positionThisFrameRigthHand = rightHand.transform.localPosition;
+        positionThisFrameLeftHand = new Vector3(0, leftHand.transform.localPosition.y, leftHand.transform.localPosition.z);
+        positionThisFrameRigthHand =  new Vector3(0, rightHand.transform.localPosition.y, rightHand.transform.localPosition.z);
         
 
         //Get distance the hands have moved since the last frame
@@ -51,6 +62,8 @@ public class HandSwinging : MonoBehaviour
         //Add them up to get the hand speed from the user
         handSpeed = ((leftHandDistanceMoved) +
                      (rightHandDistanceMoved)) * 2f;
+        
+        //speed = highSpeedModeAccelerationCurve.Evaluate(Time.realtimeSinceStartup);
 
         if (Time.timeSinceLevelLoad > 1f)
             transform.position += forwardDirection.transform.forward * (handSpeed * speed * Time.deltaTime);
