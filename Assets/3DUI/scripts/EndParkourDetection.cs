9fc04d4f7fb530d9255fbcb876b340c2f4e29553
melina-heinisch <melina.heinisch@stud-mail.uni-wuerzburg.de>
2023-08-31 11:43:14 +0200

    Added obstacles and decoration to course

--- Assets/3DUI/scripts/EndParkourDetection.cs
+++ Assets/3DUI/scripts/EndParkourDetection.cs
@@ -2,11 +2,8 @@ using UnityEngine;
 
 public class EndParkourDetection : MonoBehaviour
 {
-    [SerializeField] private GameObject ui;
     [SerializeField] private GameLogic GameLogic;
-    public VRHostSystem VRHostSystem;
 
-    
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Something touched The End Barrier:" + other.name);
@@ -17,7 +14,6 @@ public class EndParkourDetection : MonoBehaviour
             GetComponent<MeshRenderer>().enabled = false;
             GetComponent<BoxCollider>().enabled = false;
             transform.Find("Text (TMP)").gameObject.SetActive(false);
-            SetUiPosition();
             GameLogic.timerActive = false;
             if (GameLogic.timeRemaining > 0)
                 GameLogic.isWin = true;
@@ -31,13 +27,4 @@ public class EndParkourDetection : MonoBehaviour
         transform.Find("Text (TMP)").gameObject.SetActive(true);
     }
 
-    public void SetUiPosition()
-    {
-        Transform playerHead = VRHostSystem.getXROriginGameObject().transform;
-        float distanceFromHead = 1f;
-        Vector3 targetPosition = playerHead.position + playerHead.forward * distanceFromHead;
-        targetPosition.y = 4;
-        ui.transform.position = targetPosition;
-        ui.transform.rotation = playerHead.rotation;
-    }
 }
